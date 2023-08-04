580edb4d61a17750c657f01f443259a37228e414
Adrian Rubens <132026576+AdrianRibeiroo@users.noreply.github.com>
2023-08-04 07:41:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function multipica(a,b)
+{
+    return a*b;
+}
+let x = parseInt(prompt('Digite um número'));
+let y = parseInt(prompt('Digite um número'));
+
+let z = multipica(x,y);
+alert(`Resultado: ${z}`);
